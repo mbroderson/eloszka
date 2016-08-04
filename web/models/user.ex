2091811ce8d5ed0f3446de4a8d61dc4e03937cc3
mbroderson <m.brodecki@binarapps.com>
2016-08-04 14:32:28 +0200

    Basic user model with registration form

---
+++ web/models/user.ex
@@ -0,0 +1,17 @@
+defmodule Eloszka.User do
+  use Eloszka.Web, :model
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    timestamps
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name email), [])
+    # |> unique_constraint(:email)
+    # |> validate_format(:email, ~r/@binarapps.com/)
+  end
+end
---
+++ web/views/user_view.ex
@@ -0,0 +1,3 @@
+defmodule Eloszka.UserView do
+  use Eloszka.Web, :view
+end
