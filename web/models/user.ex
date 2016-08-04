42174e4aaf5277f918c6b397faf1940226b60834
mbroderson <m.brodecki@binarapps.com>
2016-08-04 15:52:15 +0200

    Using comeonin to hash the password

--- web/models/user.ex
+++ web/models/user.ex
@@ -14,4 +14,21 @@ defmodule Eloszka.User do
     |> unique_constraint(:email)
     |> validate_format(:email, ~r/@binarapps.com/)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 1, max: 20)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
